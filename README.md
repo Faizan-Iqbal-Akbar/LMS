ccd2b0f8db2dfe6d866bf1e88f1102774ffe2388
Faizan-Iqbal-Akbar <faizan.iqbal.akbar@gmail.com>
2024-05-29 05:06:38 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+
+# Brilliant Pro LMS
+Brilliant Pro is a proven, trusted and flexible tool-set 
+to support both blended learning and 100% online courses. 
+It delivers a powerful set of learner-centric tools and 
+collaborative learning environments that empower teaching 
+as well as learning, and are simple to understand and easy to use.
+The primary purpose of this LMS is to effectively create and manage e-Learning courses ,learning
+material and assessment. Therefore, administrators will construct and manage courses and content.
+
+
+## Working
+The Brilliant Pro LMS works in two different modes for 
+Admins and Learners respectively. 
+
+• Learners are the end-users. These are the users for whom the 
+training content is designed. They are the recipients of the 
+training and access the LMS to view learning material and
+participate in the course work and assessments, all while 
+tracking their own progress along the way.
+
+• Administrators are in charge for the management of the LMS, 
+which includes multiple tasks ranging from course creation, 
+to learner assignment, to monitoring learner progress.
+
+
+## Preview
+![Landing Page](./src/assests/img/landingpage.PNG)
---
+++ server/midlleware/authenticate.js
@@ -0,0 +1,47 @@
+const { verify } = require('jsonwebtoken');
+const Admin = require('../model/admin');
+const Learner = require('../model/learner');
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const { private_key } = process.env;
+
+module.exports = {
+    adminToken: catchAsync(async (req, res, next) => {
+        const token = req.headers.authorization;
+        if(!token) {
+            return next(new AppError('No token provided', 401));
+        }
+        const foundAdmin = await Admin.findOne({accesstoken: token});
+        if(!foundAdmin) {
+            return next(new AppError('Invalid token', 401));
+        }
+        verify(token, private_key, (err, _) => {
+            if(err && err.name === 'TokenExpiredError') {
+                return next(new AppError('Token expired', 401));
+            }
+            else if(err && err.name === 'JsonWebTokenError') {
+                return next(new AppError('Invalid token', 401));
+            }
+            next();
+        });
+    }),
+    learnerToken: catchAsync(async (req, res, next) => {
+        const token = req.headers.authorization;
+        if(!token) {
+            return next(new AppError('No token provided', 401));
+        }
+        const foundLearner = await Learner.findOne({accesstoken: token});
+        if(!foundLearner) {
+            return next(new AppError('Invalid token', 401));
+        }
+        verify(token, private_key, (err, _) => {
+            if(err && err.name === 'TokenExpiredError') {
+                return next(new AppError('Token expired', 401));
+            }
+            else if(err && err.name === 'JsonWebTokenError') {
+                return next(new AppError('Invalid token', 401));
+            }
+            next();
+        });
+    })
+}
